8bae9852667a60143618d77f46e62e73c2d86967
sohyeon-zoe-kim <hlna030838@gmail.com>
2023-02-07 23:09:07 +0900

    study: boolean, number, string type

---
+++ basic-type/boolean.js
@@ -0,0 +1,8 @@
+"use strict";
+let isDone = false;
+isDone = true;
+console.log(typeof isDone);
+let isOk = true;
+let isNotOk = false;
+// new Boolean(true)을 사용하면 error 발생
+// Boolean 형식은 boolean형식에 할당할 수 없기 때문임
---
+++ basic-type/boolean.ts
@@ -0,0 +1,10 @@
+let isDone: boolean = false
+
+isDone = true
+
+console.log(typeof isDone)
+
+let isOk: Boolean = true
+let isNotOk: boolean = false
+// new Boolean(true)을 사용하면 error 발생
+// Boolean 형식은 boolean형식에 할당할 수 없기 때문임
